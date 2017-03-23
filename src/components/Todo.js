be8cbcc401e956533e71df5999624524a6584072
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-23 13:24:08 +0700

    event add handleChangeText (use state)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,30 +1,31 @@
 import React, {Component} from 'react';
 
 export class Todo extends Component {
+
+    state = {
+        inputText: '',
+    }
+
+    handleChangeText = (event) => {
+        this.setState({inputText: event.target.value});
+    }
+
     render() {
         return (
             <div className="card clearfix">
                 <div className="to-do-box">
                     <h1 className="title">To-do-list</h1>
                     <div className="form-box">
-                        <input className="input-todo" placeholder="what do you want to do?" type="text"/>
+                        <input
+                            className="input-todo"
+                            onChange={this.handleChangeText}
+                            value={this.state.inputText}
+                            placeholder="what do you want to do?"
+                            type="text"/>
                         <button className="bb-input button">Add</button>
                     </div>
-
-                    <div className="list-box">
-                        <div className="list-item">
-                            <div className="text-list">To do item 1</div>
-                            <div className="bb-action">x</div>
-                        </div>
-                        <div className="list-item">
-                            <div className="text-list">To do item 2</div>
-                            <div className="bb-action">x</div>
-                        </div>
-                    </div>
+                    <h2>{this.state.inputText}</h2>
                 </div>
-                <div className="tab-page">Page 1</div>
-                <div className="second tab-page">Page 2</div>
-                <div className="tab-page">Page 3</div>
             </div>
 
         )
