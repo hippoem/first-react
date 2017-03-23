cefe370b3db730c0e008fb12b93df219ee5c2e71
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-23 13:18:23 +0700

    add static jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
+import './Todo.css';
 
 import {Todo} from './components/Todo';
 
@@ -8,7 +10,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Todo/>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-6">
+              <Todo/>
+            </div>
+            <div className="col-md-3"></div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,8 +3,28 @@ import React, {Component} from 'react';
 export class Todo extends Component {
     render() {
         return (
-            <div>
-                <h1>Todo Component</h1>
+            <div className="card clearfix">
+                <div className="to-do-box">
+                    <h1 className="title">To-do-list</h1>
+                    <div className="form-box">
+                        <input className="input-todo" placeholder="what do you want to do?" type="text"/>
+                        <button className="bb-input button">Add</button>
+                    </div>
+
+                    <div className="list-box">
+                        <div className="list-item">
+                            <div className="text-list">To do item 1</div>
+                            <div className="bb-action">x</div>
+                        </div>
+                        <div className="list-item">
+                            <div className="text-list">To do item 2</div>
+                            <div className="bb-action">x</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="tab-page">Page 1</div>
+                <div className="second tab-page">Page 2</div>
+                <div className="tab-page">Page 3</div>
             </div>
 
         )
