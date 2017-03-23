fabc943b20b8739a18dfbb203e634761a45a258c
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-23 14:12:10 +0700

    add react-router
    "react-router": "^2.7.0"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 import {Todo} from './components/Todo';
+
+import { Link } from 'react-router'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="container">
-          <div className="row">
-            <div className="col-md-3">
-              <Todo buttonColor="red"/>
+            <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-md-6">
+                  <div className="card clearfix">
+                    <Link to="/page1">
+                      <div className="tab-page">Page 1</div>
+                    </Link>
+                    <Link to="/page2">
+                      <div className="second tab-page">Page 2</div>
+                    </Link>
+                    <Link to="/page3">
+                      <div className="tab-page">Page 3</div>
+                    </Link>
+                  </div>
+                  <Todo buttonColor="green"/>
+
+                  {this.props.children}
+
+                </div>
+                <div className="col-md-3"></div>
             </div>
-            <div className="col-md-6">
-              <Todo buttonColor="green"/>
-            </div>
-            <div className="col-md-3">
-              <Todo buttonColor="blue"/>
-            </div>
-          </div>
         </div>
       </div>
     );
