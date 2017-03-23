8cfa9fd2efec179c835e64f5400e6987edfe82f4
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-23 12:08:18 +0700

    create Todo.js with create new component

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo Component</h1>
+            </div>
+
+        )
+    }
+}
