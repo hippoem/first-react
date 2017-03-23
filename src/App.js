ea107541bb4fe823874ff3065ba0a855e6ec3425
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-23 13:34:54 +0700

    event add to-do-list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './bootstrap.css';
-import './Todo.css';
 
 import {Todo} from './components/Todo';
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
+import '../bootstrap.css';
+import '../Todo.css';
 
 export class Todo extends Component {
 
     state = {
         inputText: '',
+        listItem: []
     }
 
     handleChangeText = (event) => {
         this.setState({inputText: event.target.value});
+    }
+
+    submitList = () => {
+        this.setState({
+            listItem: this.state.listItem.concat([this.state.inputText]),
+            inputText:''
+        })
+    }
+
+    handleKeyPress = (event) => {
+        if (event.key === 'Enter'){
+            this.submitList();
+        }
     }
 
     render() {
@@ -22,9 +38,15 @@ export class Todo extends Component {
                             value={this.state.inputText}
                             placeholder="what do you want to do?"
                             type="text"/>
-                        <button className="bb-input button">Add</button>
+                        <button className="bb-input button" onClick={this.submitList}>Add</button>
                     </div>
-                    <h2>{this.state.inputText}</h2>
+                    {
+                        this.state.listItem.map((value, index) => {
+                            return (
+                                <h3 key={index + value}>{value}</h3>
+                            );
+                        })
+                    }
                 </div>
             </div>
 
